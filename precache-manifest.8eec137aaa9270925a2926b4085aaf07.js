7a9d73ccc37fd500de2711d4380807aa91266253
Partha <softaweb13@gmail.com>
2020-07-15 18:06:04 +0530

    react uplad

---
+++ precache-manifest.8eec137aaa9270925a2926b4085aaf07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5830bff19e6fe363c032a34f6c2e7b2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a2969e793e897c4a9c5",
+    "url": "/static/css/main.50504919.chunk.css"
+  },
+  {
+    "revision": "b402c6917e6c6ef07957",
+    "url": "/static/js/2.5f39c59e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5f39c59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2969e793e897c4a9c5",
+    "url": "/static/js/main.e0152157.chunk.js"
+  },
+  {
+    "revision": "98f2c80171bdb807d3c8",
+    "url": "/static/js/runtime-main.7842bae7.js"
+  }
+]);
